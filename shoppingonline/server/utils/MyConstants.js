e24afa5bf3cb511413315f0253190f448c2ba3d1
TLLoc <130376661+TLLoc@users.noreply.github.com>
2024-07-21 13:08:18 +0700

    First time uploading

---
+++ shoppingonline/server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'tuanloc.hzgkrdc.mongodb.net',
+    DB_USER: 'TuanLoc',
+    DB_PASS: 'LeTuanLoc611',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: 'KHONG_CO_KHOA',
+    JWT_EXPIRES: '341123123123123', // in milliseconds
+    EMAIL_USER: 'ltuanloc2002@gmail.com', // gmail service
+    EMAIL_PASS: 'tmlxlpdbouorqmay'
+  };
+  module.exports = MyConstants;
